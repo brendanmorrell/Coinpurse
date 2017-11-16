d7a58cf6ddf5ece4d092be6367e805dae05a8322
Brendan Morrell <brendanmorrell@gmail.com>
2017-11-16 14:21:57 -0700

    no real change

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -27,7 +27,7 @@ export default class ExpenseForm extends React.Component {
   };
   onAmountChange = (e) => {
     const amount = e.target.value;
-    if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
+    if (!amount || !!amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
       return this.setState(() => ({ amount, errorAmount: '' }));
     }
     return undefined;
